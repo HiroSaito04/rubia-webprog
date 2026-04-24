0e6ee9a109ca977bd62182f5817462035d8b83b5
HiroSaito04 <sungjinwoo4444@gmail.com>
2026-04-25 06:14:23 +0800

    lab-act4-scrolltotop-and-fullwidthlayout

--- rubia-client/src/layouts/Layout.jsx
+++ rubia-client/src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrolltoTop';
 
 const Layout = () => {
   return (
-    <div className="min-h-screen bg-zinc-100 text-zinc-900">
+    <div className="min-h-ascreen bg-zinc-100 text-zinc-900">
+      <ScrollToTop />
       <NavBar />
       <main className="pb-16 pt-20">
         <Outlet />
